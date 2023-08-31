cda5a6152c1f427524d4e09e204bbccfc7f70910
Sander Johansen <sander.olin@gmail.com>
2023-08-31 20:29:24 +0200

    Finished first task: linear regression 2d

--- assignment-1/linear-regression-2d/main.py
+++ assignment-1/linear-regression-2d/main.py
@@ -1,44 +1,34 @@
 import torch
 import matplotlib.pyplot as plt
-import csv
+import pandas as pd
 
-data = []
-with open('length_weight.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        data.append(row)
-
-x_train = torch.tensor([float(row['# length']) for row in data]).reshape(-1, 1)
-y_train = torch.tensor([float(row['weight']) for row in data]).reshape(-1, 1)
+data = pd.read_csv("../data/length_weight.csv")
+x_train = torch.tensor(data["# length"].values, dtype=torch.float).reshape(-1, 1)
+y_train = torch.tensor(data['weight'].values, dtype=torch.float).reshape(-1, 1)
 
 
 class LinearRegressionModel:
 
     def __init__(self):
-        self.W = torch.tensor([[0.0]], requires_grad=True)  # requires_grad enables calculation of gradients
-        self.b = torch.tensor([[0.0]], requires_grad=True)
+        self.W = torch.tensor([[0.0]], dtype=torch.float, requires_grad=True)
+        self.b = torch.tensor([[0.0]], dtype=torch.float, requires_grad=True)
 
     # Predictor
     def f(self, x):
-        return x @ self.W + self.b  # @ corresponds to matrix multiplication
+        return x @ self.W + self.b
 
     # Uses Mean Squared Error
     def loss(self, x, y):
-        return torch.mean(torch.square(self.f(x) - y))  # Can also use torch.nn.functional.mse_loss(self.f(x), y)
-        # to possibly increase numerical stability
+        return torch.mean(torch.square(self.f(x) - y))
 
 
 model = LinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.SGD([model.W, model.b], 0.01)
-for epoch in range(1000):
+optimizer = torch.optim.SGD([model.W, model.b], 0.0001)
+for epoch in range(300000):
     model.loss(x_train, y_train).backward()  # Compute loss gradients
     optimizer.step()  # Perform optimization by adjusting W and b,
-    # similar to:
-    # model.W -= model.W.grad * 0.01
-    # model.b -= model.b.grad * 0.01
-
     optimizer.zero_grad()  # Clear gradients for next step
 
 # Print model variables and loss
