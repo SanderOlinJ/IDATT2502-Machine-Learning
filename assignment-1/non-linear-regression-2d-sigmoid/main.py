5107c26af475f263cd5e0b893246a6a1f01dcc2a
Sander Johansen <sander.olin@gmail.com>
2023-09-01 02:51:29 +0200

    Finished third task: non-linear regression 2d with sigmoid

--- assignment-1/non-linear-regression-2d-sigmoid/main.py
+++ assignment-1/non-linear-regression-2d-sigmoid/main.py
@@ -2,12 +2,12 @@ import torch
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("../data/length_weight.csv")
-x_train = torch.tensor(data["# length"].values, dtype=torch.float).reshape(-1, 1)
-y_train = torch.tensor(data['weight'].values, dtype=torch.float).reshape(-1, 1)
+data = pd.read_csv("../data/day_head_circumference.csv")
+x_train = torch.tensor(data["# day"].values, dtype=torch.float).reshape(-1, 1)
+y_train = torch.tensor(data['head circumference'].values, dtype=torch.float).reshape(-1, 1)
 
 
-class LinearRegressionModel:
+class NonLinearRegressionModel:
 
     def __init__(self):
         self.W = torch.tensor([[0.0]], dtype=torch.float, requires_grad=True)
@@ -15,17 +15,17 @@ class LinearRegressionModel:
 
     # Predictor
     def f(self, x):
-        return x @ self.W + self.b
+        return 20 * torch.sigmoid(x @ self.W + self.b) + 31
 
     # Uses Mean Squared Error
     def loss(self, x, y):
         return torch.mean(torch.square(self.f(x) - y))
 
 
-model = LinearRegressionModel()
+model = NonLinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.SGD([model.W, model.b], 0.0001)
+optimizer = torch.optim.SGD([model.W, model.b], 0.000001)
 for epoch in range(300000):
     model.loss(x_train, y_train).backward()  # Compute loss gradients
     optimizer.step()  # Perform optimization by adjusting W and b,
@@ -35,10 +35,11 @@ for epoch in range(300000):
 print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
 
 # Visualize result
+plt.xlabel('x days')
+plt.ylabel('y head circumference')
 plt.plot(x_train, y_train, 'o', label='$(x^{(i)},y^{(i)})$')
-plt.xlabel('x')
-plt.ylabel('y')
-x = torch.tensor([[torch.min(x_train)], [torch.max(x_train)]])
-plt.plot(x, model.f(x).detach(), label='$f(x) = xW+b$')
+x = torch.arange(torch.min(x_train), torch.max(x_train), 1.0).reshape(-1, 1)
+y = model.f(x).detach()
+plt.plot(x, y, label='$f(x) = 20σ(xW+b)+31$')
 plt.legend()
 plt.show()
